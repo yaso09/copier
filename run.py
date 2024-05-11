0347e775ae2eb2211fc4031696446aa8d8487794
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2024-05-11 15:06:28 +0300

    fixed bugs

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from main import app
 
-http_server = WSGIServer("0.0.0.0", app)
+http_server = WSGIServer(("127.0.0.1", 3000), app)
 http_server.serve_forever()
